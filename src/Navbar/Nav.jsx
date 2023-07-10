c87220d3bbd51bc476fd6014d81d95d9ad9bca3a
aditya123473892 <117269123+aditya123473892@users.noreply.github.com>
2023-07-10 20:53:36 +0530

    i was checking commit

--- src/Navbar/Nav.jsx
+++ src/Navbar/Nav.jsx
@@ -7,10 +7,10 @@ export default function Nav() {
         <h2> Data Verse    </h2>
             
         <ul>
-           <h4> <li>home        </li></h4>
-           <h4>   <li>about us    </li></h4>
-            <h4>   <li> contact us </li></h4>
-          <h4>  <li> help       </li></h4>
+        <h4> <li>home        </li></h4>
+        <h4>  <li>about us    </li></h4>
+        <h4> <li> contact us </li></h4>
+        <h4> <li> help       </li></h4>
         </ul>
     </div>
   )
