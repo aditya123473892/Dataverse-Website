5585459be2b81da069634f4360ee4f9570f375ee
aditya123473892 <117269123+aditya123473892@users.noreply.github.com>
2023-07-10 20:52:39 +0530

    i made a navbar

---
+++ src/Navbar/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './nav.css'
+export default function Nav() {
+  return (
+    <div className='container'>
+        
+        <h2> Data Verse    </h2>
+            
+        <ul>
+           <h4> <li>home        </li></h4>
+           <h4>   <li>about us    </li></h4>
+            <h4>   <li> contact us </li></h4>
+          <h4>  <li> help       </li></h4>
+        </ul>
+    </div>
+  )
+}
